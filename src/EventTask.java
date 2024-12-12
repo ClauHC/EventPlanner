81ea1c62c5bcc09d78636b7fc128b3118d7d32e6
ClauHC <clauhc90@gmail.com>
2024-12-12 21:20:36 +0100

    Initial commit

---
+++ src/EventTask.java
@@ -0,0 +1,17 @@
+
+public class EventTask {
+
+    //Atributos
+    private String text;
+    private boolean isCompleted;
+
+    /** Métod para marcar la tarea como completada
+     *
+     */
+
+
+    /** Métod toString para mostrar los detalles de la tarea
+     *
+     */
+
+}
---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    /**Métod principal Main
+     * se hace una llamada al nétod inicio para correr el programa
+     * @param args
+     */
+    public static void main(String[] args) {
+        Main programa = new Main();
+        programa.inicio();
+    }
+
+    /**Metdo inicio
+     * métodoo que se llama en el main. Aqui va lo que iría en el Main normalmente. Cambio necesario para poder usar programacion modular.
+     * No necesita parametros de entrada
+     */
+    public void inicio(){
+
+    }
+
+
+
+    /**Metod del menú
+     *
+     *
+     */
+    public void menuEventPlanner () {
+        //Menú de opciones
+        Scanner scanner = new Scanner(System.in);
+        int numMenu;
+
+        do {
+            // Mostrar el menú
+            System.out.println("\nBienvenido a Event Planner. Seleccione una opción");
+            System.out.println("[1] Añadir evento");
+            System.out.println("[2] Borrar evento");
+            System.out.println("[3] Listar eventos");
+            System.out.println("[4] Marcar/desmarcar tarea de un evento como completada");
+            System.out.println("[5] Salir");
+
+            // Verificar si la entrada es numérica
+            while (!scanner.hasNextInt()) {
+                System.out.println("Opción inválida. Por favor, ingresa un número.");
+                scanner.next();
+            }
+            numMenu = scanner.nextInt();
+
+            // Procesar la opción seleccionada
+            switch (numMenu) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intenta de nuevo.");
+            }
+        } while (numMenu != 4);
+    }
+
+}
