a68e8ecbbcf411f3153341221462de7f608f08d7
ClauHC <clauhc90@gmail.com>
2024-12-13 17:02:26 +0100

    Completing the agregarEvento Method

--- src/HernandezClaudiaMain.java
+++ src/HernandezClaudiaMain.java
@@ -58,22 +58,22 @@ public class HernandezClaudiaMain {
             switch (numMenu) {
 
                 case 1:
-                    System.out.println("\n Vamos a añadir un evento");
+                    System.out.println("\nVamos a añadir un evento");
                     agregarEvento(scanner);
                     break;
 
                 case 2:
-                    System.out.println("\n Vamos a borrar un evento");
+                    System.out.println("\nVamos a borrar un evento");
                     borrarEvento(scanner);
                     break;
 
                 case 3:
-                    System.out.println("\n Vamos a ver el listado de eventos que hay");
+                    System.out.println("\nVamos a ver el listado de eventos que hay");
                     listarEventos();
                     break;
 
                 case 4:
-                    System.out.println("\n Quieres modificar una tarea");
+                    System.out.println("\nQuieres modificar una tarea");
                     modificarTareas(scanner);
                     break;
 
@@ -94,6 +94,22 @@ public class HernandezClaudiaMain {
      */
     private void agregarEvento(Scanner scanner) {
 
+        System.out.print("Introduce el nombre del evento: ");
+        String title = scanner.next();
+        scanner.nextLine();
+
+        System.out.print("Introduce la fecha del evento (AAAA-MM-DD): ");
+        String textofecha = scanner.next();
+        LocalDate date = LocalDate.parse(textofecha);
+
+        System.out.print("Selecciona la prioridad (1 = HIGH, 2 = MEDIUM, 3 = LOW): ");
+        int numPrioridad = scanner.nextInt();
+        Priority prioridad = Priority.values()[numPrioridad - 1];
+
+        HernandezClaudiaEvent evento = new HernandezClaudiaEvent(title, date, prioridad);
+        evento.addTask(String.valueOf(evento));
+
+        System.out.println(" Evento: " + evento + "\n !Añadido con éxito!");
     }
 
     /**Metdo para borrar eventos
